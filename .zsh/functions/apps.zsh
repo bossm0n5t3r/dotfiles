dc22d1319059c6dd3af25b6d7e6028e0617ad927
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-12-22 20:00:00 +0900

    feat: add sdk-upgrade-all function to automate SDKMAN updates

--- .zsh/functions/apps.zsh
+++ .zsh/functions/apps.zsh
@@ -1,12 +1,17 @@
 oapp() {
-  open -a "$1"
+    open -a "$1"
 }
 
 qapp() {
-  pkill -x "$1"
+    pkill -x "$1"
 }
 
 reset-launchpad() {
-  rm -rf /private"$(getconf DARWIN_USER_DIR)"com.apple.dock.launchpad
-  killall Dock
+    rm -rf /private"$(getconf DARWIN_USER_DIR)"com.apple.dock.launchpad
+    killall Dock
 }
+
+sdk-upgrade-all() {
+    sdk selfupdate && sdk upgrade
+}
+
